8256d2469dea4e66ff5cf67e4597cc6b4bb991c0
robbiebriceno <robbi.briceno@tecsup.edu.pe>
2025-04-02 16:42:06 -0600

    robbie test v2

--- quiz/urls.py
+++ quiz/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('exam/<int:exam_id>/', views.exam_detail, name='exam_detail'),
     path('exam/create/', views.exam_create, name='exam_create'),
     path('exam/<int:exam_id>/question/add/', views.question_create, name='question_create'),
+    path('exam/<int:exam_id>/update/', views.exam_update, name='exam_update'),
+    path('exam/<int:exam_id>/delete/', views.exam_delete, name='exam_delete'),
+    path('exam/<int:exam_id>/solve/', views.exam_solve, name='exam_solve'),
 ]
 
--- quiz/views.py
+++ quiz/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.forms import inlineformset_factory
 from django.db import transaction
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView, DeleteView
+from django.http import HttpResponseRedirect
 from .models import Exam, Question, Choice
 from .forms import ExamForm, QuestionForm, ChoiceFormSet
 
@@ -69,3 +72,50 @@ def question_create(request, exam_id):
         'question_form': question_form,
         'formset': formset,
     })
+
+def exam_update(request, exam_id):
+    """Vista para actualizar un examen existente"""
+    exam = get_object_or_404(Exam, id=exam_id)
+    
+    if request.method == 'POST':
+        form = ExamForm(request.POST, instance=exam)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Examen actualizado correctamente.')
+            return redirect('exam_detail', exam_id=exam.id)
+    else:
+        form = ExamForm(instance=exam)
+    
+    return render(request, 'quiz/exam_form.html', {'form': form, 'exam': exam})
+
+
+def exam_delete(request, exam_id):
+    """Vista para eliminar un examen"""
+    exam = get_object_or_404(Exam, id=exam_id)
+    
+    if request.method == 'POST':
+        exam.delete()
+        messages.success(request, 'Examen eliminado correctamente.')
+        return redirect('exam_list')
+    
+    return render(request, 'quiz/exam_confirm_delete.html', {'exam': exam})
+
+def exam_solve(request, exam_id):
+    """Vista para resolver un examen"""
+    exam = get_object_or_404(Exam, id=exam_id)
+    questions = exam.questions.all().prefetch_related('choices')
+    
+    if request.method == 'POST':
+        # Procesar las respuestas enviadas por el usuario
+        score = 0
+        for question in questions:
+            selected_choice_id = request.POST.get(f'question_{question.id}')
+            if selected_choice_id:
+                selected_choice = question.choices.filter(id=selected_choice_id, is_correct=True).first()
+                if selected_choice:
+                    score += 1
+        
+        messages.success(request, f'Has completado el examen. Tu puntuación es {score}/{questions.count()}.')
+        return redirect('exam_list')
+    
+    return render(request, 'quiz/exam_solve.html', {'exam': exam, 'questions': questions})
